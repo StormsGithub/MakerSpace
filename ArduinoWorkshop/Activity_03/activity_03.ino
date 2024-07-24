1099d6fe1772e57c5a41d98291deb571a25ac666
StormsGithub <120608442+StormsGithub@users.noreply.github.com>
2024-07-24 10:38:55 +1000

    Update activity_03.ino
    Updated the pin number for each colour channel as pin 12 (green channel) did not utilise PWM on our Arduino's.

--- ArduinoWorkshop/Activity_03/activity_03.ino
+++ ArduinoWorkshop/Activity_03/activity_03.ino
@@ -1,7 +1,7 @@
-// Using Digital Pins 12, 11, and 10 to control the RGB LED
-int LED_R_Channel = 10;
-int LED_B_Channel = 11;
-int LED_G_Channel = 12;
+// Using Digital Pins 11, 10, and 9 to control the RGB LED
+int LED_R_Channel = 9;
+int LED_B_Channel = 10;
+int LED_G_Channel = 11;
 
 void setup()
 {
